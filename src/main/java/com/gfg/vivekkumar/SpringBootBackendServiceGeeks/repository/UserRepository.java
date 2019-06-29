3396a85de032490512c055e4c9f213fa2fd3d2db
dell <vivekofficial2018@gmail.com>
2019-06-29 06:23:16 +0530

    annotate user repository

--- src/main/java/com/gfg/vivekkumar/SpringBootBackendServiceGeeks/repository/UserRepository.java
+++ src/main/java/com/gfg/vivekkumar/SpringBootBackendServiceGeeks/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.gfg.vivekkumar.SpringBootBackendServiceGeeks.repository;
 
 import com.gfg.vivekkumar.SpringBootBackendServiceGeeks.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
