240b65e131af1b4a1d355b23904dca2bfa8d45b1
dell <vivekofficial2018@gmail.com>
2019-06-29 05:38:07 +0530

    downloaded skeleton freom spring initializr

---
+++ src/main/java/com/gfg/vivekkumar/SpringBootBackendServiceGeeks/SpringBootBackendServiceGeeksApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.vivekkumar.SpringBootBackendServiceGeeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendServiceGeeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendServiceGeeksApplication.class, args);
+	}
+
+}
